2bfaeac2eaed96c98fb9868e6c05daaa58f362b3
goForItkang <kangyohan.dev.0421@gmail.com>
2025-09-04 20:15:58 +0900

    이메일 중복처리 하는중 -yh

--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router =express.Router();
 const debug = require('debug')('app:login');
 const pool = require('../config/mysql');
+
 // login post
 router.post('/login', (req, res) => {
     const {email,password} = req.body;
@@ -14,11 +15,32 @@ router.post('/login', (req, res) => {
     }
 });
 // 회원 가입 로직
-//
 router.post('/signup', async (req, res) => {
     const {email,password,name} = req.body;
     console.log('회원 가입 정보',req.body);
     await pool.query('insert into users(email,password,name,created_at) values(?,?,?,now())',[email,password,name])
     res.redirect('/login')
+})
+// 이메일 중복확인 로직
+router.post('/dublicate', async (req, res)=>{
+    const {email} = req.body;
+    // 확인
+    debug('dublicate email', email);
+    // 만약 비어 있지 않고
+
+    if(req.body != null) {
+       const [rows] = await pool.query('select * from users where email = ?', [email]);
+        debug('dublicate email', rows);
+        if(rows.length > 0) {
+            // 중복된 아이디가 있을경우
+            return res.status(400).json(
+                {available:false}
+            )
+        }else{
+            return res.status(200).json(
+                {available:true}
+            )
+        }
+    }
 })
 module.exports = router;
